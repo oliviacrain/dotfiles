b090c569da31646db25e45d05a4e9a7675d1b924
Olivia Crain <olivia@olivia.dev>
2023-12-22 15:22:02 -0600

    Use unstable tailscale on cardinalis

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,18 +1,25 @@
-{
-  inputs,
-  outputs,
-  lib,
-  config,
-  pkgs,
-  user,
-  ...
+{ inputs
+, outputs
+, lib
+, config
+, pkgs
+, user
+, ...
 }: {
   imports = [
     ./hardware-configuration.nix
   ];
 
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-  nixpkgs.config.allowUnfree = true;
+  nixpkgs = {
+    overlays = [
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+    ];
+    config.allowUnfree = true;
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   environment.systemPackages = with pkgs; [
     kitty
@@ -36,10 +43,10 @@
 
   users.users.olivia = {
     isNormalUser = true;
-    extraGroups = ["wheel" "networkmanager" ];
+    extraGroups = [ "wheel" "networkmanager" ];
     home = "/home/olivia";
     openssh.authorizedKeys.keys = [
-          "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIAcHy4OLWLYz3mrLJRPtJjSbuB0ovD2rKDrKzxjQmgSwAAAABHNzaDo= olivia@olivia.dev"
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIAcHy4OLWLYz3mrLJRPtJjSbuB0ovD2rKDrKzxjQmgSwAAAABHNzaDo= olivia@olivia.dev"
     ];
   };
 
@@ -52,15 +59,18 @@
     openFirewall = true;
   };
   services.vscode-server.enable = true;
-  services.tailscale.enable = true;
+  services.tailscale = {
+    enable = true;
+    package = pkgs.unstable.tailscale;
+  };
   networking.firewall = {
     enable = true;
-    trustedInterfaces = ["tailscale0"];
-    allowedUDPPorts = [config.services.tailscale.port];
-    allowedTCPPorts = [22];
+    trustedInterfaces = [ "tailscale0" ];
+    allowedUDPPorts = [ config.services.tailscale.port ];
+    allowedTCPPorts = [ 22 ];
   };
 
-  services.xserver.videoDrivers = ["nvidia"];
+  services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = false;
