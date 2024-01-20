c3b5a5c68048f523351fa3618d12c126ce23a329
Olivia Crain <olivia@olivia.dev>
2024-01-20 14:38:11 -0600

    pkgs: add caddy-with-porkbun

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,10 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
-pkgs: {
-  # example = pkgs.callPackage ./example { };
+{pkgs, ...}: {
+  caddy-with-porkbun = pkgs.callPackage ./caddy-with-plugins.nix {
+    vendorHash = "sha256-tR9DQYmI7dGvj0W0Dsze0/BaLjG84hecm0TPiCVSY2Y=";
+    externalPlugins = [
+      { name = "porkbun"; repo = "github.com/caddy-dns/porkbun"; version = "v0.1.4"; }
+    ];
+  };
 }
