74517f07c4135ffbd0e403829b5724184ce21b92
Olivia Crain <olivia@olivia.dev>
2024-02-23 12:43:56 -0600

    hm: remove unfree workaround

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -15,8 +15,6 @@
     ];
     config = {
       allowUnfree = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/2942
-      allowUnfreePredicate = _: true;
     };
   };
 
