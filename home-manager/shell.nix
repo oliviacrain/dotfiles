52ad8d9b03d7a0c40bc97612a73fc06832636411
Olivia Crain <olivia@olivia.dev>
2024-02-26 11:01:10 -0600

    hm: add atuin, zellij

--- home-manager/shell.nix
+++ home-manager/shell.nix
@@ -3,10 +3,10 @@
   programs.bat.enable = true;
   programs.bash.enable = true;
   programs.alacritty.enable = true;
+  programs.atuin.enable = true;
+  programs.zellij.enable = true;
   programs.fzf.enable = true;
-  programs.nushell = {
-    enable = true;
-  };
+  programs.nushell.enable = true; 
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
