dfc7e10be5f3607637ae2224366ae7c29bac2de7
Olivia Crain <olivia@olivia.dev>
2024-02-26 11:23:04 -0600

    hm: alphabetize shell

--- home-manager/shell.nix
+++ home-manager/shell.nix
@@ -1,15 +1,15 @@
 { ... }:
 {
-  programs.bat.enable = true;
-  programs.bash.enable = true;
   programs.alacritty.enable = true;
   programs.atuin.enable = true;
-  programs.zellij.enable = true;
-  programs.fzf.enable = true;
-  programs.nushell.enable = true; 
-  programs.jq.enable = true;
+  programs.bat.enable = true;
+  programs.bash.enable = true;
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
   };
+  programs.fzf.enable = true;
+  programs.jq.enable = true;
+  programs.nushell.enable = true; 
+  programs.zellij.enable = true;
 }
