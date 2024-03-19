e3002283b7926b5232e2075c02407e82ee3aed71
Olivia Crain <olivia@olivia.dev>
2024-03-19 00:27:25 -0500

    nixos: add clamav, calibre to all systems

--- nixos/common/packages.nix
+++ nixos/common/packages.nix
@@ -1,11 +1,21 @@
 { lib, pkgs, ... }:
+let
+  inherit (builtins) attrValues;
+in
 {
 
-  environment.systemPackages = [
-    pkgs.firefox
-    pkgs.kdePackages.neochat
-    pkgs.kdePackages.tokodon
-  ];
+  environment.systemPackages = attrValues {
+    inherit (pkgs)
+      firefox
+      clamav
+      calibre
+      ;
+  } ++ attrValues {
+    inherit (pkgs.kdePackages)
+      neochat
+      tokodon
+      ;
+  };
 
   fonts.packages = [ pkgs.berkeley-mono ];
 }
