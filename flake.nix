8fcafdf68fbefe2e500218735731af10d0ab694b
Olivia Crain <olivia@olivia.dev>
2024-05-27 15:19:24 -0500

    Catfood lix main

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     lix = {
-      url = "git+https://git@git.lix.systems/lix-project/lix?ref=refs/tags/2.90-beta.1";
+      url = "git+https://git@git.lix.systems/lix-project/lix?ref=refs/heads/main";
       flake = false;
     };
 
