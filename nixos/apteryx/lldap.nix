78169936f62a75f5c8a92516ced7ddc4cf89a68c
Olivia Crain <olivia@olivia.dev>
2024-07-06 12:21:10 -0500

    apteryx: try LoadCredential with lldap

--- nixos/apteryx/lldap.nix
+++ nixos/apteryx/lldap.nix
@@ -2,10 +2,6 @@
 {
   services.lldap = {
     enable = true;
-    environment = {
-      LLDAP_JWT_SECRET_FILE = config.sops.secrets."lldap/jwt_secret".path;
-      LLDAP_LDAP_USER_PASS_FILE = config.sops.secrets."lldap/admin_user_pass".path;
-    };
     settings = {
       ldap_base_dn = "dc=slug,dc=gay";
       ldap_user_dn = "admin";
@@ -16,14 +12,17 @@
     };
   };
 
-  sops.secrets."lldap/jwt_secret" = {
-    mode = "0400";
-    owner = config.systemd.services.lldap.serviceConfig.User;
-    group = config.systemd.services.lldap.serviceConfig.Group;
+  systemd.services.lldap.serviceConfig = {
+    LoadCredential = [
+      "jwt_secret:${config.sops.secrets."lldap/jwt_secret".path}"
+      "admin_user_pass:${config.sops.secrets."lldap/admin_user_pass".path}"
+    ];
+    Environment = [
+      "LLDAP_JWT_SECRET_FILE=%d/jwt_secret"
+      "LLDAP_LDAP_USER_PASS_FILE=%d/admin_user_pass"
+    ];
   };
-  sops.secrets."lldap/admin_user_pass" = {
-    mode = "0400";
-    owner = config.systemd.services.lldap.serviceConfig.User;
-    group = config.systemd.services.lldap.serviceConfig.Group;
-  };
+
+  sops.secrets."lldap/jwt_secret" = {};
+  sops.secrets."lldap/admin_user_pass" = {};
 }
