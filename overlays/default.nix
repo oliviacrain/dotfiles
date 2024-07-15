ded99a3fda293cef83367770d95d18035620d3a8
Olivia Crain <olivia@olivia.dev>
2024-07-15 12:43:07 -0500

    overlays: patch out jellyseerr cached dns lookups

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, lib, ... }:
 {
   additions = final: _prev: import ../pkgs { pkgs = final; };
 
@@ -8,6 +8,17 @@
     # });
       inherit (inputs.nixpkgs-patched-clamav.legacyPackages.${prev.system})
         clamav;
+
+      jellyseerr = prev.jellyseerr.overrideAttrs(oldAttrs: {
+        patches = ( oldAttrs.patches or [] ) ++ [
+          (
+            prev.fetchpatch {
+              url = "https://github.com/Fallenbagel/jellyseerr/commit/268c7df28eea8b911d6a53297f5ce296983067ce.patch";
+              hash = lib.fakeHash;
+            }
+          )
+        ];
+      });
     };
 
   apple-silicon = inputs.apple-silicon.overlays.apple-silicon-overlay;
