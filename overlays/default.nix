47f7e6e72a2ed2d4eb058088f68babc2169fe40e
Olivia Crain <olivia@olivia.dev>
2024-07-15 20:43:24 -0500

    jellyseerr -> 1.9.0

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,40 +6,43 @@
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
     # });
-      clamav = prev.clamav.overrideAttrs(oldAttrs: {
-          checkInputs = (oldAttrs.checkInputs or []) ++ [
-            final.python3.pkgs.pytest
-          ];
-      });
+    clamav = prev.clamav.overrideAttrs (oldAttrs: {
+      checkInputs = (oldAttrs.checkInputs or [ ]) ++ [ final.python3.pkgs.pytest ];
+    });
 
-      jellyseerr = prev.jellyseerr.overrideAttrs(oldAttrs: {
-        patches = ( oldAttrs.patches or [] ) ++ [
-          (
-            prev.fetchpatch {
-              url = "https://github.com/Fallenbagel/jellyseerr/commit/268c7df28eea8b911d6a53297f5ce296983067ce.patch";
-              hash = "sha256-3MUdF9KgYcP/szNF4H4OiaHvm2D5EUa/y5yFsvh/Zpw=";
-            }
-          )
-        ];
-      });
-
-      python311 = prev.python311.override {
-        packageOverrides = pfinal: pprev: {
-          extruct = pprev.extruct.overridePythonAttrs (oldAttrs: {
-            version = "0.17.0";
-            src = final.fetchFromGitHub {
-              owner = "scrapinghub";
-              repo = "extruct";
-              rev = "refs/tags/v0.17.0";
-              hash = "sha256-CfhIqbhrZkJ232grhHxrmj4H1/Bq33ZXe8kovSOWSK0=";
-            };
-          });
+    jellyseerr = prev.jellyseerr.overrideAttrs (
+      oldAttrs:
+      let
+        v = "1.9.0";
+      in
+      {
+        version = v;
+        src = prev.fetchFromGitHub {
+          owner = "Fallenbagel";
+          repo = "jellyseerr";
+          rev = "v${v}";
+          hash = prev.lib.fakeHash;
         };
-      };
+      }
+    );
 
-      # https://github.com/NixOS/nixpkgs/issues/326296
-      mealie = prev.mealie.override { python3Packages = final.python311Packages; };
+    python311 = prev.python311.override {
+      packageOverrides = pfinal: pprev: {
+        extruct = pprev.extruct.overridePythonAttrs (oldAttrs: {
+          version = "0.17.0";
+          src = final.fetchFromGitHub {
+            owner = "scrapinghub";
+            repo = "extruct";
+            rev = "refs/tags/v0.17.0";
+            hash = "sha256-CfhIqbhrZkJ232grhHxrmj4H1/Bq33ZXe8kovSOWSK0=";
+          };
+        });
+      };
     };
+
+    # https://github.com/NixOS/nixpkgs/issues/326296
+    mealie = prev.mealie.override { python3Packages = final.python311Packages; };
+  };
 
   apple-silicon = inputs.apple-silicon.overlays.apple-silicon-overlay;
   widevine = inputs.widevine.overlays.default;
