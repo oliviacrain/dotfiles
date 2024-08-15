ddc3a5bf9d55814e331f1cd9002972829eac1bda
Olivia Crain <olivia@olivia.dev>
2024-08-15 16:51:06 -0500

    overlays: remove mealie py311 modifications

--- overlays/default.nix
+++ overlays/default.nix
@@ -20,35 +20,6 @@
         };
       }
     );
-
-    python311 = prev.python311.override {
-      packageOverrides = pfinal: pprev: {
-        extruct = pprev.extruct.overridePythonAttrs (oldAttrs: {
-          version = "0.17.0";
-          src = final.fetchFromGitHub {
-            owner = "scrapinghub";
-            repo = "extruct";
-            rev = "refs/tags/v0.17.0";
-            hash = "sha256-CfhIqbhrZkJ232grhHxrmj4H1/Bq33ZXe8kovSOWSK0=";
-          };
-        });
-      };
-    };
-
-    # https://github.com/NixOS/nixpkgs/issues/326296
-    mealie =
-      (prev.mealie.overrideAttrs (oldAttrs: {
-        patches =
-          (oldAttrs.patches or [])
-          ++ [
-            (prev.fetchpatch {
-              url = "https://patch-diff.githubusercontent.com/raw/mealie-recipes/mealie/pull/3882.patch";
-              hash = "sha256-/NFAxeKJ/6UJqSb/iD3ACehiTF9WF0TPB4M3kB1yYL8=";
-            })
-          ];
-      }))
-      .override
-      {python3Packages = final.python311Packages;};
   };
 
   apple-silicon = inputs.apple-silicon.overlays.apple-silicon-overlay;
