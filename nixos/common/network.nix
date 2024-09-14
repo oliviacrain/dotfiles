2096edae6778c444b5c0f5cd06c15472baa44394
Olivia Crain <olivia@olivia.dev>
2024-09-14 12:52:59 -0500

    nixos: switch to systemd-{networkd,resolved}

--- nixos/common/network.nix
+++ nixos/common/network.nix
@@ -8,12 +8,12 @@ in {
   options.olivia.network.enable = mkEnableOption "common network settings";
 
   config = mkIf config.olivia.network.enable {
-    networking.networkmanager.enable = mkDefault true;
-    networking.networkmanager.wifi.backend = mkDefault "iwd";
+    networking.useNetworkd = mkDefault true;
+    services.resolved.enable = true;
     networking.wireless.iwd.enable = mkDefault true;
-    networking.wireless.iwd.settings.General.EnableNetworkConfiguration = mkDefault true;
-
-    # https://github.com/NixOS/nixpkgs/issues/180175
-    systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
+    networking.wireless.iwd.settings.General = {
+      EnableNetworkConfiguration = mkDefault true;
+      UseDefaultInterface = mkDefault true;
+    };
   };
 }
