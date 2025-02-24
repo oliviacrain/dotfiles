5a63c486b17c33d888340d16a471444b86a72441
Olivia Crain <olivia@olivia.dev>
2025-02-24 14:00:41 -0600

    athene: only patch amdgpu module, not whole kernel

--- nixos/athene/framework.nix
+++ nixos/athene/framework.nix
@@ -3,17 +3,18 @@
   lib,
   ...
 }: let
-  backlight_min_patch = {
-    name = "backlight-min-quirk";
-    patch = pkgs.fetchpatch2 {
-      url = "https://lore.kernel.org/lkml/20240610-amdgpu-min-backlight-quirk-v1-1-8459895a5b2a@weissschuh.net/raw";
-      hash = "sha256-tXxI+G9nNc+p4y8ITISe7EioCtETtePpeuCr+oWT/+4=";
-    };
+  kernel = pkgs.linux_latest;
+  backlight_min_patch = pkgs.fetchpatch2 {
+    url = "https://lore.kernel.org/lkml/20240610-amdgpu-min-backlight-quirk-v1-1-8459895a5b2a@weissschuh.net/raw";
+    hash = "sha256-tXxI+G9nNc+p4y8ITISe7EioCtETtePpeuCr+oWT/+4=";
   };
 in {
   boot = {
-    kernelPackages = pkgs.linuxPackagesFor pkgs.linux_latest;
-    kernelPatches = [backlight_min_patch];
+    kernelPackages = pkgs.linuxPackagesFor kernel;
+    extraModulePackages = [ (pkgs.callPackage ./amdgpu-module.nix {
+      inherit kernel;
+      patches = [ backlight_min_patch ];
+    })];
   };
 
   services.fwupd.enable = true;
