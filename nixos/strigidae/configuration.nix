721c3eef64d568147b5682e71773f120c65d8add
Olivia Crain <olivia@olivia.dev>
2025-03-22 00:21:46 -0500

    strigidae: update ip, add prometheus

--- nixos/strigidae/configuration.nix
+++ nixos/strigidae/configuration.nix
@@ -31,7 +31,7 @@
     serverSettings = let
       certDir = config.security.acme.certs."auth.slug.gay".directory;
     in {
-      bindaddress = "159.65.243.4:443";
+      bindaddress = "104.131.167.151:443";
       domain = "auth.slug.gay";
       origin = "https://auth.slug.gay";
       tls_chain = "${certDir}/fullchain.pem";
@@ -66,5 +66,40 @@
         PORKBUN_SECRET_API_KEY=${config.sops.placeholder.porkbunSecretApiKey}
       '';
     };
+  };
+
+  services.prometheus = {
+    enable = true;
+    exporters.node = {
+      enable = true;
+      enabledCollectors = ["systemd"];
+    };
+    scrapeConfigs = [
+      {
+        job_name = "node";
+        static_configs = [
+          {
+            targets = [
+              "localhost:${toString config.services.prometheus.exporters.node.port}"
+            ];
+          }
+        ];
+      }
+    ];
+    remoteWrite = [
+      {
+        url = "https://prometheus-prod-36-prod-us-west-0.grafana.net/api/prom/push";
+        basic_auth = {
+          username = "1710577";
+          password_file = config.sops.secrets."prometheus/grafana_basicauth_pass".path;
+        };
+      }
+    ];
+  };
+
+  sops.secrets."prometheus/grafana_basicauth_pass" = {
+    owner = "prometheus";
+    group = "prometheus";
+    mode = "0440";
   };
 }
