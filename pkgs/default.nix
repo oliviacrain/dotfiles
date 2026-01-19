f474c04bcf14db67fc728623c81aeeee8f34b3d5
Olivia Crain <olivia@olivia.dev>
2026-01-19 12:11:31 -0600

    update caddy hash

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,7 +3,7 @@
 in {
   caddy-augmented = pkgs.caddy.withPlugins {
     plugins = ["github.com/caddy-dns/porkbun@v0.3.1"];
-    hash = "sha256-aVSE8y9Bt+XS7+M27Ua+ewxRIcX51PuFu4+mqKbWFwo=";
+    hash = "sha256-R1ZqQ8drcBQIH7cLq9kEvdg9Ze3bKkT8IAFavldVeC0=";
   };
 
   witchhazel = callPackage ./witchhazel.nix {};
