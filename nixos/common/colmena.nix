43d1ce24bc7b3296e38f8a291bd66e24821fcfc2
Olivia Crain <olivia@olivia.dev>
2026-02-06 14:02:50 -0600

    nixos: add colmena-deploy to ssh AllowUsers

--- nixos/common/colmena.nix
+++ nixos/common/colmena.nix
@@ -1,19 +1,13 @@
 {
   lib,
-  pkgs,
   config,
-  inputs,
-  outputs,
   ...
 }:
 let
   inherit (lib)
-    mkDefault
     mkEnableOption
     mkIf
-    mkAfter
     ;
-  inherit (builtins) attrValues;
   username = "colmena-deploy";
 in
 {
@@ -27,30 +21,31 @@ in
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC87Km2/XgeYLt9C164Bo6eCRS5iPaI4hCRLPGo9Nw3b colmena deploy"
       ];
     };
-    users.groups.${username} = {};
+    users.groups.${username} = { };
 
     security.sudo = {
       extraRules = [
-      {
-        users = [ username ];
-        commands = [
-          {
-            command = "/run/current-system/sw/bin/nix-store --no-gc-warning --realise /nix/store/*";
-            options = [ "NOPASSWD" ];
-          }
-          {
-            command = "/run/current-system/sw/bin/nix-env --profile /nix/var/nix/profiles/system --set /nix/store/*";
-            options = [ "NOPASSWD" ];
-          }
-          {
-            command = "/nix/store/*/bin/switch-to-configuration *";
-            options = [ "NOPASSWD" ];
-          }
-        ];
-      }
-        ];
-      };
+        {
+          users = [ username ];
+          commands = [
+            {
+              command = "/run/current-system/sw/bin/nix-store --no-gc-warning --realise /nix/store/*";
+              options = [ "NOPASSWD" ];
+            }
+            {
+              command = "/run/current-system/sw/bin/nix-env --profile /nix/var/nix/profiles/system --set /nix/store/*";
+              options = [ "NOPASSWD" ];
+            }
+            {
+              command = "/nix/store/*/bin/switch-to-configuration *";
+              options = [ "NOPASSWD" ];
+            }
+          ];
+        }
+      ];
+    };
 
     nix.settings.trusted-users = [ username ];
+    services.openssh.settings.AllowUsers = [ username ];
   };
 }
